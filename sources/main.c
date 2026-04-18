26b721b71dcd0a67445c851ddf23229814894173
Mijael334 <mzambranabernal868@alumno.unlam.edu.ar>
2026-04-18 18:32:39 -0300

    Creacion de repositorio

---
+++ sources/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    // Llamar a la carga del ABB desde el archivo binario (Integrante 3).
+
+    // Mostrar menú y pedir nombre (Integrante 4).
+
+    // Cargar configuración y generar lista circular (Integrante 1 y 3).
+
+    // Bucle de juego:
+
+    // Tirar dado y elegir dirección (Integrante 2).
+
+    // Encolar movimientos (Integrante 1).
+
+    // Desencolar y actualizar posiciones (Integrante 1 y 2).
+
+    // Refrescar pantalla (Integrante 4).
+
+    // Guardar resultados y mostrar historial (Integrante 3 y 4).
+    return 0;
+}
