eae78fccfc5f5bd56cd5e2307c23a82f373b5b53
lire25 <riverplate_del26@hotmail.com>
2026-05-09 21:23:05 -0300

    funcione de lectura de configuracion

--- include/gestion_archivos.h
+++ include/gestion_archivos.h
@@ -1,6 +1,24 @@
 #ifndef GESTION_ARCHIVOS_H_INCLUDED
 #define GESTION_ARCHIVOS_H_INCLUDED
+///defino algunas macro que podemos que deSpues se puede cambiar para su mejor manejo
+#define ERROR_ARCHIVO_CONFIG -2
+#define TODO_OK 1
+#define TAM_MAX 200
+#include <stdio.h>
+#include <string.h>
+/// talvez podria usar signed(ya que solo podriamos usar num > 0)
+typedef struct
+{
+    unsigned cant_posiciones;
+    unsigned vidas_incio;
+    unsigned bandidos_max;
+    unsigned premios_max;
+    unsigned max_vidas_extras;
+    unsigned oasis_max;
+    unsigned tormenta_max;
 
+} tconfig;
 
-
+int abrir_archivo(FILE **pf, const char *nombre_archivo, const char *modo);
+int lectura_de_configuracion(const char *archivo_config, tconfig *parametros);
 #endif // GESTION_ARCHIVOS_H_INCLUDED
