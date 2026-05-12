a7b4f01a98ce0ccde4556b5fd1f22dce8012641d
Mijael334 <mzambranabernal868@alumno.unlam.edu.ar>
2026-05-11 21:05:41 -0300

    Definida estructura de jugador

--- include/entidades.h
+++ include/entidades.h
@@ -1,6 +1,23 @@
 #ifndef ENTIDADES_H_INCLUDED
 #define ENTIDADES_H_INCLUDED
 
+#define MAX_NOMBRE 10
 
+typedef struct 
+{
+
+    unsigned vidas;
+    int protegido;
+    unsigned puntos;
+    unsigned posEnRuta;
+} tEstado_Jugador;
+
+typedef struct 
+{
+    unsigned id;
+    char nombre [MAX_NOMBRE + 1];
+    unsigned puntos;
+    tEstado_Jugador estadoPartida;
+} tJugador;
 
 #endif // ENTIDADES_H_INCLUDED
