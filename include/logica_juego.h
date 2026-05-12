25e5d092de0d3c8c609257a0675d0b7bf2650ced
Mijael334 <mzambranabernal868@alumno.unlam.edu.ar>
2026-05-11 21:06:16 -0300

    Definida parte de la estructura de juego

--- include/logica_juego.h
+++ include/logica_juego.h
@@ -1,6 +1,18 @@
 #ifndef LOGICA_JUEGO_H_INCLUDED
 #define LOGICA_JUEGO_H_INCLUDED
 
+#include "../include/entidades.h"
 
+#define MAX_DADO 6
+
+typedef struct 
+{
+    int corriendo;
+    tJugador jugador;
+    //tPartida partida; tengo que pensar que datos va a tener
+}tJuego;
+
+
+int generarValorDado ();
 
 #endif // LOGICA_JUEGO_H_INCLUDED
