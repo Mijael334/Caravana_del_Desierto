8490b7c32ad8403ea75d4bb0a21e47af6811a5d7
lire25 <riverplate_del26@hotmail.com>
2026-06-01 22:03:52 -0300

    Biblioteca de Arbol

--- include/arbol.h
+++ include/arbol.h
@@ -1,6 +1,25 @@
 #ifndef ARBOL_H_INCLUDED
 #define ARBOL_H_INCLUDED
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define DATO_NO_ENCONTRADO 0
+#define DATO_ENCONTRADO 1
+#define DATO_DUPLICADO -1
+#define SIN_MEMORIA -2
+#define TODO_OK 1
+typedef struct sNodoArbol
+{
+    void *info;
+    unsigned tamInfo;
+    struct sNodoArbol *der,
+                      *izq;
+}tNodoArbol;
+typedef tNodoArbol *tArbolBinBusq;
 
+void crearArbolBinBusq(tArbolBinBusq *pt);
+int insertarArbolBinBusq(tArbolBinBusq *pt, const void *d, unsigned cantbyte, int(*cmp)(const void *a1, const void *a2));
+int buscarEnArbolBinBusq(const tArbolBinBusq *pt, const void *d, unsigned cantbyte,int(*cmp)(const void *a1, const void *a2));
 #endif // ARBOL_H_INCLUDED
