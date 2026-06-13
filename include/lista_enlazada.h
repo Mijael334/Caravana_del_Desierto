12b5d51d663d60bb67d2ea5f367792ac9859747b
Jeremias Portilla Martinez <jerepmartinez@gmail.com>
2026-06-13 14:12:55 -0300

    primitivas de lista enlazada

---
+++ include/lista_enlazada.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_ENLAZADA_H_INCLUDED
+#define LISTA_ENLAZADA_H_INCLUDED
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MIN(X, Y) ((X) <= (Y) ? (X) : (Y))
+
+#define ERROR_MEM -1
+#define ERROR_VACIO -2
+#define OK 1
+
+typedef struct sNodoLista
+{
+    void *info;
+    unsigned tamInfo;
+    struct sNodoLista *sig;
+}tNodoLista;
+
+typedef tNodoLista *tLista;
+
+void crearLista(tLista *pl);
+int listaVacia(const tLista *pl);
+int insertarEnOrdenLista(tLista *pl, const void *d, unsigned tam, int (*comparar)(const void *, const void *));
+void vaciarLista(tLista *pl);
+void mostrarLista(const tLista *pl, void (*mostrar)(const void *));
+
+#endif // LISTA_ENLAZADA_H_INCLUDED
---
+++ sources/lista_enlazada.c
@@ -0,0 +1,56 @@
+#include "..../include/lista_enlazada.h"
+
+void crearLista(tLista *pl)
+{
+    *pl = NULL;
+}
+
+int listaVacia(const tLista *pl)
+{
+    return *pl == NULL;
+}
+
+int insertarEnOrdenLista(tLista *pl, const void *d, unsigned tam, int (*comparar)(const void *, const void *))
+{
+    tNodoLista *nue;
+    while(*pl && comparar((*pl)->info, d) > 0)
+    {
+        pl = &(*pl)->sig;
+    }
+    nue = (tNodoLista *)malloc(sizeof(tNodoLista));
+    if(nue == NULL)
+    {
+        return ERROR_MEM;
+    }
+    nue->info = malloc(tam);
+    if(nue->info == NULL)
+    {
+        free(nue);
+        return ERROR_MEM;
+    }
+    memcpy(nue->info, d, tam);
+    nue->tamInfo = tam;
+    nue->sig = *pl;
+    *pl = nue;
+    return OK;
+}
+
+void vaciarLista(tLista *pl)
+{
+    while(*pl)
+    {
+        tNodoLista *aux = *pl;
+        *pl = aux->sig;
+        free(aux->info);
+        free(aux);
+    }
+}
+
+void mostrarLista(const tLista *pl, void (*mostrar)(const void *))
+{
+    while(*pl)
+    {
+        mostrar((*pl)->info);
+        pl = &(*pl)->sig;
+    }
+}
